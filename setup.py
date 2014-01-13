dd03b21b57944589096a96f79ccde4e8db4ae91f
Carlos Thompson <git@ewey.co>
2014-01-13 11:25:07 -0500

    Setup sccript

---
+++ setup.py
@@ -0,0 +1,14 @@
+import sys
+from cx_Freeze import setup, Executable
+
+build_exe_options = {
+#    "packages": ["os"],
+    'include_files': ["winxp.cfg","win7.cfg"],
+#	'build_exe': {'include_msvcr': "C:\\WINDOWS\system32\\"},
+    }
+
+setup ( name = "MChron Reports",
+        version = "0.0.5",
+        description = "Report generator and emailer for Oruga Amarilla",
+        options = {"build_exe": build_exe_options},
+        executables = [Executable("mchron.py"),Executable("config.py")] )
