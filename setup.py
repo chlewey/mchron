9bb273af0851c306bcc028b51d6d1f9792064d3b
Carlos Thompson <git@ewey.co>
2014-02-12 10:16:25 -0500

    setup packages

--- setup.py
+++ setup.py
@@ -2,14 +2,38 @@ import sys
 from cx_Freeze import setup, Executable
 
 build_exe_options = {
-#    "packages": ["os"],
-    'packages': ['email.mime.multipart','email.mime.base','email.mime.text'],
+    'packages': [
+        'email.mime.multipart',
+        'email.mime.base',
+        'email.mime.text',
+        'reportlab.pdfbase._fontdata_enc_winansi',
+        'reportlab.pdfbase._fontdata_enc_macroman',
+        'reportlab.pdfbase._fontdata_enc_standard',
+        'reportlab.pdfbase._fontdata_enc_symbol',
+        'reportlab.pdfbase._fontdata_enc_zapfdingbats',
+        'reportlab.pdfbase._fontdata_enc_pdfdoc',
+        'reportlab.pdfbase._fontdata_enc_macexpert',
+        'reportlab.pdfbase._fontdata_widths_courier',
+        'reportlab.pdfbase._fontdata_widths_courierbold',
+        'reportlab.pdfbase._fontdata_widths_courierboldoblique',
+        'reportlab.pdfbase._fontdata_widths_courieroblique',
+        'reportlab.pdfbase._fontdata_widths_helvetica',
+        'reportlab.pdfbase._fontdata_widths_helveticabold',
+        'reportlab.pdfbase._fontdata_widths_helveticaboldoblique',
+        'reportlab.pdfbase._fontdata_widths_helveticaoblique',
+        'reportlab.pdfbase._fontdata_widths_symbol',
+        'reportlab.pdfbase._fontdata_widths_timesroman',
+        'reportlab.pdfbase._fontdata_widths_timesbold',
+        'reportlab.pdfbase._fontdata_widths_timesbolditalic',
+        'reportlab.pdfbase._fontdata_widths_timesitalic',
+        'reportlab.pdfbase._fontdata_widths_zapfdingbats',
+        ],
     'include_files': ["winxp.cfg","win7.cfg"],
-#	'build_exe': {'include_msvcr': "C:\\WINDOWS\system32\\"},
+#    'build_exe': {'include_msvcr': "C:\\WINDOWS\system32\\"},
     }
 
 setup ( name = "MChron Reports",
-        version = "0.0.5",
+        version = "0.0.7",
         description = "Report generator and emailer for Oruga Amarilla",
         options = {"build_exe": build_exe_options},
         executables = [Executable("mchron.py"),Executable("config.py")] )
